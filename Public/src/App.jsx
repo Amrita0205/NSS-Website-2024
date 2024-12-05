054585812b8b1961cb978baf9256f801c4745009
Satyajeet-Das <dassatyajeet123@gmail.com>
2024-12-05 17:36:00 +0530

    feat: added folder structure for frontend

---
+++ Public/src/App.jsx
@@ -0,0 +1,18 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
