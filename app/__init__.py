92a44b490de38c57514f219aae6502f7c802cb33
Shubham Dubey <itzshubhamofficial@gmail.com>
2024-12-09 16:41:00 +0530

    resolved conflict for admin and user blueprint registration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from app.extensions import mongo,cors
-# from app.routes.user_routes import user_blueprint
+from app.routes.user_routes import user_blueprint
 from app.routes.admin_routes import admin_bp
 
 def create_app():
@@ -14,6 +14,7 @@ def create_app():
     cors.init_app(app)
 
     #Register blueprints
+    app.register_blueprint(user_blueprint,url_prefix='/api/users')
     app.register_blueprint(admin_bp)
 
     return app
