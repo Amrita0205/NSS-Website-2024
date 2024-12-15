15a850efb8799e28328056a6c8d02ecd1ac79d70
Amrita <amrita0205kadam@gmail.com>
2024-12-15 19:52:38 +0530

    feat:App_event.jsx

---
+++ Public/src/App_event.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App_event.css';
+import EventDetail1 from './EventDetail1';
+
+function App_event() {
+  return (
+    <div className="App">
+      <EventDetail1></EventDetail1>
+     {/* add other events by eventdetail2, eventdetail3 and others */}
+    </div>
+  );
+}
+
+export default App_event;
+
